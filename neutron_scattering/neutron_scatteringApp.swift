f386d01a1423b260c7c117613da1616de9ab26c0
Andre Guardia <andre.guardiaq@gmail.com>
2023-02-10 16:21:01 -0600

    Initial Commit

---
+++ neutron_scattering/neutron_scatteringApp.swift
@@ -0,0 +1,17 @@
+//
+//  neutron_scatteringApp.swift
+//  neutron_scattering
+//
+//  Created by IIT Phys 440 on 2/10/23.
+//
+
+import SwiftUI
+
+@main
+struct neutron_scatteringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
